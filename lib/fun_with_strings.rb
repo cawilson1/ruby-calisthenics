67916a6b3cc4efc1d28c739a2fdafdd2c5671e31
Casey Wilson <cawilson1@g.cofc.edu>
2017-09-10 16:10:29 +0000

    dessert.rb and fun_with_strings.rb complete

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,12 +1,47 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    self.delete!("^a-zA-Z")
+    self.downcase.reverse == self.downcase
   end
   def count_words
-    # your code here
+    arr = self.split(" ")
+    arr.each{|v| v.downcase!}
+    arr.each{|v| v.gsub!(/[\W]/, '')}
+    count_hash = Hash.new(0)
+    arr.each{|v| count_hash[v]+= 1}
+    count_hash.delete('')
+    return count_hash
   end
+  
   def anagram_groups
-    # your code here
+    return [] if self == ""
+    arr = self.split(" ")
+    #array to return
+    returnArr = []
+    #elements that do have anagrams to compare to non-anagram arrays
+    anagramIsTrue = []
+    counter = 0
+    arr.each{|v1|
+      arr.each{|v2|
+        unless v1==v2
+          if v1.chars.sort.join == v2.chars.sort.join
+            returnArr[counter] = [v1,v2]
+            anagramIsTrue[counter] = v1
+            counter += 1
+          end
+        end
+      }
+    }
+    
+    arr.each{|v|  if !anagramIsTrue.include? v 
+      returnArr[counter] = [v] 
+      counter += 1
+    end
+    }
+    
+    puts returnArr
+          
+    return returnArr
   end
 end
 
