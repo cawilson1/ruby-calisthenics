ef2519d18c3554cb04bf198a08777a2acb883564
Casey Wilson <cawilson1@g.cofc.edu>
2017-09-15 12:10:13 +0000

    Finished all four exercises

--- lib/attr_accessor_with_history.rb
+++ lib/attr_accessor_with_history.rb
@@ -4,7 +4,30 @@ class Class
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+      def #{attr_name}= thing
+      
+        if (!defined?(@#{attr_name}_history))
+            @#{attr_name}_history = []
+        end
+        
+        @#{attr_name}_history.push(@#{attr_name})
+        @#{attr_name} = thing
+      end
     }
+
+
   end
 end
+
+
+class Yo
+    attr_accessor_with_history :test
+end
+
+you = Yo.new
+you.test = :wow
+you.test = "done"
+you.test = :wow
+you.test = nil
+you.test = "and"
+print you.test_history
